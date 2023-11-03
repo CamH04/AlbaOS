a629db69ae8ee1cc7fe1c24c781052b0f3d163a1
CamH04 <cameronahaynes04@gmail.com>
2023-11-03 14:12:05 +0000

    hello world

---
+++ kernel.cpp
@@ -0,0 +1,23 @@
+void printf(char* str)
+{
+    unsigned short* VideoMemoryLocation = (unsigned  short*)0xb8000;
+    for(int i= 0; str[i] != '\0'; i++){
+        VideoMemoryLocation[i] = (VideoMemoryLocation[i] & 0xFF00) | str[i];
+    }
+}
+
+typedef void (*constructor)();
+extern "C" constructor* start_ctors;
+extern "C" constructor* end_ctors;
+extern "C" void callConstructors()
+{
+    for(constructor* i = &start_ctors; i != end_ctors; i++)
+        (*i)();
+}
+
+
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+{
+    printf("Hello World!");
+    while(1);
+}

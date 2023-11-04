d640d01a47c1b378db83b1b593604c4fec6105ad
CamH04 <cameronahaynes04@gmail.com>
2023-11-04 14:10:21 +0000

    Added iso file

--- kernel.cpp
+++ kernel.cpp
@@ -7,7 +7,7 @@ void printf(char* str)
 }
 
 typedef void (*constructor)();
-extern "C" constructor* start_ctors;
+extern "C" constructor start_ctors;
 extern "C" constructor* end_ctors;
 extern "C" void callConstructors()
 {

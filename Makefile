4c32a07deb6817e8d2bc5840509b63ed5562a255
CamH04 <cameronahaynes04@gmail.com>
2023-11-05 14:20:59 +0000

    Day3 small changes

--- Makefile
+++ Makefile
@@ -21,7 +21,7 @@ mykernel.iso: mykernel.bin
 	mkdir iso/boot
 	mkdir iso/boot/grub
 	cp $< iso/boot/
-	echo 'set timeout=0' >> iso/boot/grub/grub.cfg
+	echo 'set timeout=0' > iso/boot/grub/grub.cfg
 	echo 'set default=0' >> iso/boot/grub/grub.cfg
 	echo '' >> iso/boot/grub/grub.cfg
 	echo 'menuentry "AblaOS" {' >> iso/boot/grub/grub.cfg
--- kernel.cpp
+++ kernel.cpp
@@ -1,9 +1,9 @@
+
 void printf(char* str)
 {
-    unsigned short* VideoMemoryLocation = (unsigned  short*)0xb8000;
-    for(int i= 0; str[i] != '\0'; i++){
+    unsigned short* VideoMemoryLocation = (unsigned short*)0xb8000;
+    for(int i = 0; str[i] != '\0'; i++)
         VideoMemoryLocation[i] = (VideoMemoryLocation[i] & 0xFF00) | str[i];
-    }
 }
 
 typedef void (*constructor)();
@@ -14,7 +14,6 @@ extern "C" void callConstructors()
     for(constructor* i = &start_ctors; i != end_ctors; i++)
         (*i)();
 }
-
 
 extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
 {

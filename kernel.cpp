47807747376823bbb93eddba7180da7864f2f1a2
CamH04 <cameronahaynes04@gmail.com>
2023-11-07 14:27:13 +0000

    Added Print Line

--- kernel.cpp
+++ kernel.cpp
@@ -1,12 +1,49 @@
 #include "types.h"
 #include "gdt.h"
 
-void printf(char* str)
+void justincasethingsbreak(char* str)
 {
     unsigned short* VideoMemoryLocation = (unsigned short*)0xb8000;
     for(int i = 0; str[i] != '\0'; i++)
         VideoMemoryLocation[i] = (VideoMemoryLocation[i] & 0xFF00) | str[i];
 }
+void printf(char* str)
+{
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+
+    static uint8_t x=0,y=0;
+
+    for(int i = 0; str[i] != '\0'; ++i)
+    {
+        switch(str[i])
+        {
+            case '\n':
+                x = 0;
+                y++;
+                break;
+            default:
+                VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
+                x++;
+                break;
+        }
+
+        if(x >= 80)
+        {
+            x = 0;
+            y++;
+        }
+
+        if(y >= 25)
+        {
+            for(y = 0; y < 25; y++)
+                for(x = 0; x < 80; x++)
+                    VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+            x = 0;
+            y = 0;
+        }
+    }
+}
+
 
 typedef void (*constructor)();
 extern "C" constructor start_ctors;
@@ -48,6 +85,8 @@ extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
     printf("                           ################## ");
     printf("");
     printf("");
+
+    GlobalDescriptorTable gdt;
     while(1);
 }
 

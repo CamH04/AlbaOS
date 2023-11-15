b96635a62b872a6aa46a456afce20432fc2d0238
CamH04 <cameronahaynes04@gmail.com>
2023-11-15 16:27:14 +0000

    Trying To not get Vm to crash because of interrupts last try of the day

--- interruptstubs.s
+++ interruptstubs.s
@@ -1,14 +1,13 @@
-
 .set IRQ_BASE, 0x20
 
 .section .text
 
 .extern _ZN16InterruptManager15HandleInterruptEhj
 
-
-
 .macro HandleException num
 .global _ZN16InterruptManager19HandleException\num\()Ev
+
+
 _ZN16InterruptManager19HandleException\num\()Ev:
     movb $\num, (interruptnumber)
     jmp int_bottom
@@ -17,7 +16,6 @@ _ZN16InterruptManager19HandleException\num\()Ev:
 
 .macro HandleInterruptRequest num
 .global _ZN16InterruptManager26HandleInterruptRequest\num\()Ev
-
 
 _ZN16InterruptManager26HandleInterruptRequest\num\()Ev:
     movb $\num + IRQ_BASE, (interruptnumber)
@@ -63,14 +61,49 @@ HandleInterruptRequest 0x0E
 HandleInterruptRequest 0x0F
 HandleInterruptRequest 0x31
 
-
 int_bottom:
+
+    # register sichern
     pusha
     pushl %ds
     pushl %es
     pushl %fs
     pushl %gs
 
+    # ring 0 segment register laden
+    #cld
+    #mov $0x10, %eax
+    #mov %eax, %eds
+    #mov %eax, %ees
+
+    # C++ Handler aufrufen
+    pushl %esp
+    push (interruptnumber)
+    call _ZN16InterruptManager15HandleInterruptEhj
+    add %esp, 6
+    mov %eax, %esp # den stack wechselnint_bottom:
+
+    # register sichern
+    pusha
+    pushl %ds
+    pushl %es
+    pushl %fs
+    pushl %gs
+
+    # ring 0 segment register laden
+    #cld
+    #mov $0x10, %eax
+    #mov %eax, %eds
+    #mov %eax, %ees
+
+    # C++ Handler aufrufen
+    pushl %esp
+    push (interruptnumber)
+    call _ZN16InterruptManager15HandleInterruptEhj
+    add %esp, 6
+    mov %eax, %esp # den stack wechseln
+
+    # register laden
     pop %gs
     pop %fs
     pop %es

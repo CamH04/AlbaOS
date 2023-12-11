c2e1e15f37907cc9f5cd65c8ed6c7cd59aa6cf76
CamH04 <cameronahaynes04@gmail.com>
2023-12-11 12:05:52 +0000

    fixing ports gpf3

--- kernel.cpp
+++ kernel.cpp
@@ -70,7 +70,7 @@ extern "C" void callConstructors()
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic*/)
 {
     //cool stuff
-    printowl();
+    printf("hello");
 
     //boring nerd stuff
     GlobalDescriptorTable gdt;

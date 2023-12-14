5688a63b78f4614a5acc228156eb21fcdb5f5d45
CamH04 <cameronahaynes04@gmail.com>
2023-12-14 13:26:06 +0000

    bunch of improvments for interrupts

--- interrupts.cpp
+++ interrupts.cpp
@@ -5,7 +5,41 @@
 void printf(char* str);
 
 
+//update - making it so it doesnt point to static function, insted it repoints to interruptManager so we can have pic access
+
+//implementing stuff we made in the h file
+//register itself
+InterruptHandler::InterruptHandler(InterruptManager* interruptManager, uint8_t InterruptNumber)
+{
+    this->InterruptNumber = InterruptNumber;
+    this->interruptManager = interruptManager;
+    interruptManager->handlers[InterruptNumber] = this;
+}
+//unregister itseelf
+InterruptHandler::~InterruptHandler()
+{
+    if(interruptManager->handlers[InterruptNumber] == this)
+        interruptManager->handlers[InterruptNumber] = 0;
+}
+
+uint32_t InterruptHandler::HandleInterrupt(uint32_t esp)
+{
+    return esp;
+}
+
+
+
+
+
+
+
+
+
+
 InterruptManager::GateDescriptor InterruptManager::interruptDescriptorTable[256];
+InterruptManager* InterruptManager::ActiveInterruptManager = 0;
+
+
 
 
 void InterruptManager::SetInterruptDescriptorTableEntry(uint8_t interrupt,
@@ -36,10 +70,10 @@ InterruptManager::InterruptManager(uint16_t hardwareInterruptOffset, GlobalDescr
     for(uint8_t i = 255; i > 0; --i)
     {
         SetInterruptDescriptorTableEntry(i, CodeSegment, &InterruptIgnore, 0, IDT_INTERRUPT_GATE);
-        //handlers[i] = 0;
+        handlers[i] = 0;
     }
     SetInterruptDescriptorTableEntry(0, CodeSegment, &InterruptIgnore, 0, IDT_INTERRUPT_GATE);
-    //handlers[0] = 0;
+    handlers[0] = 0;
 
     SetInterruptDescriptorTableEntry(0x00, CodeSegment, &HandleException0x00, 0, IDT_INTERRUPT_GATE);
     SetInterruptDescriptorTableEntry(0x01, CodeSegment, &HandleException0x01, 0, IDT_INTERRUPT_GATE);
@@ -113,32 +147,56 @@ uint16_t InterruptManager::HardwareInterruptOffset()
 
 void InterruptManager::Activate()
 {
-    //if(ActiveInterruptManager == 0)
-    {
-        //ActiveInterruptManager = this;
-        asm("sti");
-    }
+    if(ActiveInterruptManager != 0)
+        ActiveInterruptManager->Deactivate();
+
+    ActiveInterruptManager = this;
+    asm("sti");
 }
 
 void InterruptManager::Deactivate()
 {
-    /*if(ActiveInterruptManager == this)
+    if(ActiveInterruptManager == this)
     {
         ActiveInterruptManager = 0;
-        */
-        //asm("cli");
-        /*
-    }*/
+        asm("cli");
+    }
 }
 
 uint32_t InterruptManager::HandleInterrupt(uint8_t interrupt, uint32_t esp)
 {
-    char* foo = "INTERRUPT 0x00";
-    char* hex = "0123456789ABCDEF";
+    if(ActiveInterruptManager != 0)
+        return ActiveInterruptManager->DoHandleInterrupt(interrupt, esp);
+    //printf("interrupt");
+    return esp;
+}
 
-    foo[12] = hex[(interrupt >> 4) & 0xF];
-    foo[13] = hex[interrupt & 0xF];
-    printf(foo);
+//"here we put the stuff we really do" - viktor
+uint32_t InterruptManager::DoHandleInterrupt(uint8_t interrupt, uint32_t esp)
+{
+    // doesnt output 0x20, just everything else
+    if(handlers[interrupt] != 0)
+    {
+        esp = handlers[interrupt]->HandleInterrupt(esp);
+    }
+    //if we dont have a interrupt handler
+    else if(interrupt != hardwareInterruptOffset)
+    {
+        char* foo = "UNHANDLED INTERRUPT 0x00";
+        char* hex = "0123456789ABCDEF";
+        foo[22] = hex[(interrupt >> 4) & 0xF];
+        foo[23] = hex[interrupt & 0xF];
+        printf(foo);
+    }
+
+    // replying to the interrupt, 0x20 == good yippie
+    if(hardwareInterruptOffset <= interrupt && interrupt < hardwareInterruptOffset+16)
+    {
+        programmableInterruptControllerMasterCommandPort.Write(0x20);
+        //only have to send if came from slave
+        if(hardwareInterruptOffset + 8 <= interrupt)
+            programmableInterruptControllerSlaveCommandPort.Write(0x20);
+    }
 
     return esp;
 }

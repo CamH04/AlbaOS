3afe9719da4ef76094b15b4bdbd9f8f82d7b0477
CamH04 <cameronahaynes04@gmail.com>
2024-02-14 14:29:14 +0000

    full rewritten keyboard drivers done

--- src/drivers/keyboard.cpp
+++ src/drivers/keyboard.cpp
@@ -87,46 +87,115 @@ uint32_t KeyboardDriver::HandleInterrupt(uint32_t esp)
             case 0x0A: if (handler->shift) { handler->OnKeyDown('('); } else { handler->OnKeyDown('9'); } break;
             case 0x0B: if (handler->shift) { handler->OnKeyDown(')'); } else { handler->OnKeyDown('0'); } break;
 
+            case 0x0C: if (handler->shift) { handler->OnKeyDown('_'); } else { handler->OnKeyDown('-'); } break;
+            case 0x0D: if (handler->shift) { handler->OnKeyDown('+'); } else { handler->OnKeyDown('='); } break;
 
-            case 0x10: handler->OnKeyDown('q'); break;
-            case 0x11: handler->OnKeyDown('w'); break;
-            case 0x12: handler->OnKeyDown('e'); break;
-            case 0x13: handler->OnKeyDown('r'); break;
-            case 0x14: handler->OnKeyDown('t'); break;
-            case 0x15: handler->OnKeyDown('z'); break;
-            case 0x16: handler->OnKeyDown('u'); break;
-            case 0x17: handler->OnKeyDown('i'); break;
-            case 0x18: handler->OnKeyDown('o'); break;
-            case 0x19: handler->OnKeyDown('p'); break;
+            //top row letters
+            case 0x10: if (handler->shift ^ handler->caps) { handler->OnKeyDown('Q'); } else { handler->OnKeyDown('q'); } break;
+            case 0x11: if (handler->shift ^ handler->caps) { handler->OnKeyDown('W'); } else { handler->OnKeyDown('w'); } break;
+            case 0x12: if (handler->shift ^ handler->caps) { handler->OnKeyDown('E'); } else { handler->OnKeyDown('e'); } break;
+            case 0x13: if (handler->shift ^ handler->caps) { handler->OnKeyDown('R'); } else { handler->OnKeyDown('r'); } break;
+            case 0x14: if (handler->shift ^ handler->caps) { handler->OnKeyDown('T'); } else { handler->OnKeyDown('t'); } break;
+            case 0x15: if (handler->shift ^ handler->caps) { handler->OnKeyDown('Y'); } else { handler->OnKeyDown('y'); } break;
+            case 0x16: if (handler->shift ^ handler->caps) { handler->OnKeyDown('U'); } else { handler->OnKeyDown('u'); } break;
+            case 0x17: if (handler->shift ^ handler->caps) { handler->OnKeyDown('I'); } else { handler->OnKeyDown('i'); } break;
+            case 0x18: if (handler->shift ^ handler->caps) { handler->OnKeyDown('O'); } else { handler->OnKeyDown('o'); } break;
+            case 0x19: if (handler->shift ^ handler->caps) { handler->OnKeyDown('P'); } else { handler->OnKeyDown('p'); } break;
 
-            case 0x1E: handler->OnKeyDown('a'); break;
-            case 0x1F: handler->OnKeyDown('s'); break;
-            case 0x20: handler->OnKeyDown('d'); break;
-            case 0x21: handler->OnKeyDown('f'); break;
-            case 0x22: handler->OnKeyDown('g'); break;
-            case 0x23: handler->OnKeyDown('h'); break;
-            case 0x24: handler->OnKeyDown('j'); break;
-            case 0x25: handler->OnKeyDown('k'); break;
-            case 0x26: handler->OnKeyDown('l'); break;
+            case 0x1A: if (handler->shift) { handler->OnKeyDown('{'); } else { handler->OnKeyDown('['); } break;
+            case 0x1B: if (handler->shift) { handler->OnKeyDown('}'); } else { handler->OnKeyDown(']'); } break;
 
-            case 0x2C: handler->OnKeyDown('y'); break;
-            case 0x2D: handler->OnKeyDown('x'); break;
-            case 0x2E: handler->OnKeyDown('c'); break;
-            case 0x2F: handler->OnKeyDown('v'); break;
-            case 0x30: handler->OnKeyDown('b'); break;
-            case 0x31: handler->OnKeyDown('n'); break;
-            case 0x32: handler->OnKeyDown('m'); break;
-            case 0x33: handler->OnKeyDown(','); break;
-            case 0x34: handler->OnKeyDown('.'); break;
-            case 0x35: handler->OnKeyDown('-'); break;
+            // 2nd row letters
+            case 0x1E: if (handler->shift ^ handler->caps) { handler->OnKeyDown('A'); } else { handler->OnKeyDown('a'); } break;
+            case 0x1F: if (handler->shift ^ handler->caps) { handler->OnKeyDown('S'); } else { handler->OnKeyDown('s'); } break;
+            case 0x20: if (handler->shift ^ handler->caps) { handler->OnKeyDown('D'); } else { handler->OnKeyDown('d'); } break;
+            case 0x21: if (handler->shift ^ handler->caps) { handler->OnKeyDown('F'); } else { handler->OnKeyDown('f'); } break;
+            case 0x22: if (handler->shift ^ handler->caps) { handler->OnKeyDown('G'); } else { handler->OnKeyDown('g'); } break;
+            case 0x23: if (handler->shift ^ handler->caps) { handler->OnKeyDown('H'); } else { handler->OnKeyDown('h'); } break;
+            case 0x24: if (handler->shift ^ handler->caps) { handler->OnKeyDown('J'); } else { handler->OnKeyDown('j'); } break;
+            case 0x25: if (handler->shift ^ handler->caps) { handler->OnKeyDown('K'); } else { handler->OnKeyDown('k'); } break;
+            case 0x26: if (handler->shift ^ handler->caps) { handler->OnKeyDown('L'); } else { handler->OnKeyDown('l'); } break;
 
+            case 0x2B: if (handler->shift) { handler->OnKeyDown('~'); } else { handler->OnKeyDown('#'); } break;
+            case 0x28: if (handler->shift) { handler->OnKeyDown('@'); } break;
+            case 0x27: if (handler->shift) { handler->OnKeyDown(':'); } else { handler->OnKeyDown(';'); } break;
+
+            //3rd row letters
+            case 0x2C: if (handler->shift ^ handler->caps) { handler->OnKeyDown('Z'); } else { handler->OnKeyDown('z'); } break;
+            case 0x2D: if (handler->shift ^ handler->caps) { handler->OnKeyDown('X'); } else { handler->OnKeyDown('x'); } break;
+            case 0x2E: if (handler->shift ^ handler->caps) { handler->OnKeyDown('C'); } else { handler->OnKeyDown('c'); } break;
+            case 0x2F: if (handler->shift ^ handler->caps) { handler->OnKeyDown('V'); } else { handler->OnKeyDown('v'); } break;
+            case 0x30: if (handler->shift ^ handler->caps) { handler->OnKeyDown('B'); } else { handler->OnKeyDown('b'); } break;
+            case 0x31: if (handler->shift ^ handler->caps) { handler->OnKeyDown('N'); } else { handler->OnKeyDown('n'); } break;
+            case 0x32: if (handler->shift ^ handler->caps) { handler->OnKeyDown('M'); } else { handler->OnKeyDown('m'); } break;
+
+            case 0x33: if (handler->shift) { handler->OnKeyDown('<'); } else { handler->OnKeyDown(','); } break;
+            case 0x34: if (handler->shift) { handler->OnKeyDown('>'); } else { handler->OnKeyDown('.'); } break;
+            case 0x35: if (handler->shift) { handler->OnKeyDown('?'); } else { handler->OnKeyDown('/'); } break;
+
+            // funny keys
+            //enter
             case 0x1C: handler->OnKeyDown('\n'); break;
             case 0x39: handler->OnKeyDown(' '); break;
+            //caps
+            case 0x3A: handler->caps = handler->caps ^ 1; break;
+            //win key
+            case 0x5B: break;
+            //backspace
+            case 0x0E: handler->OnKeyDown('\b'); break;
+            //esc
+            case 0x01: handler->OnKeyDown('\x1b'); break;
+            //right ctrl
+            case 0x1D: handler->ctrl = true; break;
+            case 0x9D: handler->ctrl = false; break;
+            //tab
+            case 0x0F: handler->OnKeyDown('\v'); break;
+            //alt keys
+            case 0x38: handler->alt = true; break;
+            case 0xB8: handler->alt = true; break;
+            case 0xE0: handler->alt =  false; break;
+            //left arrow
+            case 0x4B: handler->OnKeyDown('\xfc'); break;
+            //up arrow
+            case 0x48: handler->OnKeyDown('\xfd'); break;
+            //down arrow
+            case 0x50: handler->OnKeyDown('\xfe'); break;
+            //right arrow
+            case 0x4D: handler->OnKeyDown('\xff'); break;
+            //shift keys
+            case 0x2A: handler->shift = true; break;
+            case 0x36: handler->shift = true; break;
+            case 0xAA: handler->shift = false; break;
+            case 0xB6: handler->shift = false; break;
+
+            //strange keys
+            //pintscrn
+            case 0x37: break;
+            //scrollock
+            case 0x46: break;
+            //break
+            case 0x45: break;
+            //ins
+            case 0x52: break;
+            //home
+            case 0x47: break;
+            //pgup
+            case 0x49: break;
+            //del
+            case 0x53: break;
+            //end
+            case 0x4F: break;
+            //pgdown
+            case 0x51: break;
+
 
             default:
             {
+                //used for debugging keyboard, if you dont want to see hex codes just comment out the printf's
                 printf("KEYBOARD 0x");
                 printfHex(key);
+
+                handler->OnKeyUp();
                 break;
             }
         }

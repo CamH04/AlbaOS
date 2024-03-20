24771c2f03d948cdcd54a1ea3207acf545709b27
CamH04 <cameronahaynes04@gmail.com>
2024-03-20 13:03:09 +0000

    tring to fix cprintf 3

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -105,24 +105,41 @@ void console_new_line()
    }
    curx=0;
 }
-void cprintf(uint32_t colour, char *string)
+void cprintf(uint32_t colour, char *str)
 {
-    uint32_t curc=0;
+     static uint16_t *VideoMemory = (uint16_t*)0xb8000;
 
-    char *video=(char*)0xB8000;
+    static uint8_t x=0;
+    static uint8_t y=0;
 
-    video = video + curc;
-    while(*string!=0)
+    for(int i = 0; str[i] != '\0'; ++i)
     {
-        *video=*string;
-        string++;
-        video++;
-        *video=colour;
-        video++;
-        curc=curc+2;
-        if(curx==80)
+        *VideoMemory=colour;
+        switch(str[i])
         {
-         console_new_line();
+            case '\n':
+                x = 0;
+                y++;
+                break;
+            default:
+                VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
+                x++;
+                break;
+        }
+
+        if(x >= 80)
+        {
+            x = 0;
+            y++;
+        }
+
+        if(y >= 25)
+        {
+            for(y = 0; y < 25; y++)
+                for(x = 0; x < 80; x++)
+                    VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+            x = 0;
+            y = 0;
         }
     }
 }
@@ -312,8 +329,7 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot
 
     printf("Welcome To AlbaOS Version Beta 0.94");
     printf("\n");
-    console_new_line();
-    cprintf(13,"$>");
+    cprintf(13, "Test \n");
 
 
 
